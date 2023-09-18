56c6c4eab5487b2d3f9d2870a98dc6bb57b65845
abletoburntheweb <115485262+abletoburntheweb@users.noreply.github.com>
2023-09-18 21:18:27 +0300

    Add files via upload

---
+++ LR/4.10.pas
@@ -0,0 +1,9 @@
+﻿var n: integer;
+begin
+writeln ('На лугу пасется');
+readln(n);
+if (n>10) and (n<20) then writeln(n,' коров ') else
+  if (n = 0) or (n = 5) or (n = 6) or (n = 7)or (n = 8) or (n = 9) then writeln(n,' коров ') else
+    if n = 1 then writeln( n,' коров ') else
+      if (n = 2) or (n = 3) or (n = 4) then writeln(n,' коровы ');
+end.
