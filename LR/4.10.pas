94e31e7438a31a431af847d0f1e8001a7deee7f3
abletoburntheweb <115485262+abletoburntheweb@users.noreply.github.com>
2023-10-04 13:14:37 +0300

    Update 4.10.pas

--- LR/4.10.pas
+++ LR/4.10.pas
@@ -1,9 +1,10 @@
-﻿var n: integer;
+var a:integer;
 begin
-writeln ('На лугу пасется');
-readln(n);
-if (n>10) and (n<20) then writeln(n,' коров ') else
-  if (n = 0) or (n = 5) or (n = 6) or (n = 7)or (n = 8) or (n = 9) then writeln(n,' коров ') else
-    if n = 1 then writeln( n,' коров ') else
-      if (n = 2) or (n = 3) or (n = 4) then writeln(n,' коровы ');
+  writeln('Введите число до 100');
+  readln(a);
+  if (a=1) or (a=21) or (a=31) or (a=41) or (a=51) or (a=61) or (a=71) or (a=81) or (a=91) then
+    writeln(a, 'корова') else 
+  if (a>4) and (a<21) or (a>24) and (a<31) or (a>34) and (a<41) or (a>44) and (a<51) or (a>54) and (a<61) or (a>64) and (a<71) or (a>74) and (a<81) or (a>84) and (a<91) or (a>94) then
+    writeln(a, ' коров ') else
+    writeln(a, ' коровы ');
 end.
